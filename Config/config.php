78c36d425837c73c7f5d436bbcf7622a815b7c47
khoadautay <54214642+KhoaDauTay@users.noreply.github.com>
2024-01-17 21:35:50 +0700

    Update config.php

--- Config/config.php
+++ Config/config.php
@@ -9,9 +9,9 @@ define('URL_ROOT', '/');
 define('URL_SUBFOLDER', 'BitStorm');
 
 // Tham số cơ sở dữ liệu
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'DATA_PHP');
-
+define('DB_HOST', 'us-cluster-east-01.k8s.cleardb.net');
+define('DB_USER', 'bbb1a7de96f0c1');
+define('DB_PASS', '3ed99811');
+define('DB_NAME', 'heroku_3c39f03f53fadc0');
+// mysql://bbb1a7de96f0c1:3ed99811@us-cluster-east-01.k8s.cleardb.net/heroku_3c39f03f53fadc0?reconnect=true
 ?>
